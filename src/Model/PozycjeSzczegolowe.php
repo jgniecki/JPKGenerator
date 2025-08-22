5cd688e5c0944620d7e48baf79b59472aa9a257d
Jakub Gniecki <jgniecki.contact@gmail.com>
2025-08-22 11:22:32 +0200

    Dodano pola

--- src/Model/PozycjeSzczegolowe.php
+++ src/Model/PozycjeSzczegolowe.php
@@ -8,9 +8,75 @@
 
 namespace DevLancer\JPKGenerator\Model;
 
+use DevLancer\JPKGenerator\ParameterBag;
+
 final  class PozycjeSzczegolowe
 {
+    private ParameterBag $p_11;
+    private ParameterBag $p_12;
+    private ParameterBag $p_19;
+    private ParameterBag $p_20;
     public function __construct()
     {
+        $this->p_11 = new ParameterBag(
+            "P_11",
+            "float",
+        );
+        $this->p_12 = new ParameterBag(
+            "P_12",
+            "float",
+        );
+        $this->p_19 = new ParameterBag(
+            "P_19",
+            "float",
+        );
+        $this->p_20 = new ParameterBag(
+            "P_20",
+            "float",
+        );
+    }
+
+    public function getP11(): ParameterBag
+    {
+        return $this->p_11;
+    }
+
+    public function setP11(ParameterBag $p_11): static
+    {
+        $this->p_11 = $p_11;
+        return $this;
+    }
+
+    public function getP12(): ParameterBag
+    {
+        return $this->p_12;
+    }
+
+    public function setP12(ParameterBag $p_12): static
+    {
+        $this->p_12 = $p_12;
+        return $this;
+    }
+
+    public function getP19(): ParameterBag
+    {
+        return $this->p_19;
+    }
+
+    public function setP19(ParameterBag $p_19): static
+    {
+        $this->p_19 = $p_19;
+        return $this;
+    }
+
+    public function getP20(): ParameterBag
+    {
+        return $this->p_20;
+    }
+
+    public function setP20(ParameterBag $p_20): static
+    {
+        $this->p_20 = $p_20;
+        return $this;
     }
 }
