6d0edc2dbe9a34536bc7c60e5eb6e63f74e4f2db
Jakub Gniecki <kubuspl@onet.eu>
2025-08-22 09:34:16 +0200

    Add XML generator for Document

---
+++ src/XmlGenerator.php
@@ -0,0 +1,92 @@
+<?php declare(strict_types=1);
+
+namespace DevLancer\JPKGenerator;
+
+use DOMDocument;
+use DOMElement;
+use ReflectionClass;
+use DateTimeInterface;
+
+final class XmlGenerator
+{
+    private const DEFAULT_PREFIX = 'tns';
+    /**
+     * Mapping of element names to namespace prefixes
+     * @var array<string,string>
+     */
+    private array $prefixMap;
+
+    public function __construct(array $prefixMap = [])
+    {
+        // default mapping for elements that should use 'etd' prefix
+        $this->prefixMap = $prefixMap + [
+            'NIP' => 'etd',
+            'ImiePierwsze' => 'etd',
+            'Nazwisko' => 'etd',
+            'DataUrodzenia' => 'etd',
+        ];
+    }
+
+    public function generate(Document $document): string
+    {
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->formatOutput = true;
+
+        $rootBag = $document->getJpk();
+        $rootElement = $this->createElement($dom, $rootBag);
+        if ($rootElement !== null) {
+            $dom->appendChild($rootElement);
+        }
+
+        return $dom->saveXML();
+    }
+
+    private function createElement(DOMDocument $dom, ParameterBag $bag): ?DOMElement
+    {
+        $value = $bag->getValue();
+        if ($value === null) {
+            return null;
+        }
+
+        $tagName = $this->resolveTagName($bag->getTagName());
+        $element = $dom->createElement($tagName);
+
+        foreach ($bag->getAttributesList() as $attr) {
+            if ($bag->hasAttribute($attr)) {
+                $element->setAttribute($attr, (string) $bag->getAttribute($attr));
+            }
+        }
+
+        if (is_object($value)) {
+            $ref = new ReflectionClass($value);
+            foreach ($ref->getProperties() as $property) {
+                $property->setAccessible(true);
+                $childBag = $property->getValue($value);
+                if ($childBag instanceof ParameterBag) {
+                    $childElement = $this->createElement($dom, $childBag);
+                    if ($childElement !== null) {
+                        $element->appendChild($childElement);
+                    }
+                }
+            }
+        } else {
+            $element->nodeValue = $this->scalarValue($value);
+        }
+
+        return $element;
+    }
+
+    private function resolveTagName(string $name): string
+    {
+        $prefix = $this->prefixMap[$name] ?? self::DEFAULT_PREFIX;
+        return $prefix !== '' ? $prefix . ':' . $name : $name;
+    }
+
+    private function scalarValue(mixed $value): string
+    {
+        if ($value instanceof DateTimeInterface) {
+            return $value->format('Y-m-d\TH:i:s\Z');
+        }
+        return (string) $value;
+    }
+}
