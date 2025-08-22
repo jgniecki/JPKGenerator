311afb5414398836681ba72a5d05d14ba9f3f12d
Jakub Gniecki <jgniecki.contact@gmail.com>
2025-08-22 09:15:52 +0200

    Dodano nowe modele

---
+++ src/Model/Deklaracja.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+/**
+ * @author Jakub Gniecki <jgniecki.contact@gmail.com>
+ * @copyright
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DevLancer\JPKGenerator\Model;
+
+use DevLancer\JPKGenerator\ParameterBag;
+
+final class Deklaracja
+{
+    private ParameterBag $naglowek;
+    private ParameterBag $pozycjeSzczegolowe;
+    private ParameterBag $pouczenia;
+    public function __construct()
+    {
+        $this->naglowek = new ParameterBag(
+            "Naglowek",
+            NaglowekDeklaracja::class,
+            false,
+        );
+
+        $this->pozycjeSzczegolowe = new ParameterBag(
+            "PozycjeSzczegolowe",
+            PozycjeSzczegolowe::class,
+            false,
+        );
+        $this->pouczenia = new ParameterBag(
+            "Pouczenia",
+            "int",
+            false,
+        );
+    }
+
+    public function getNaglowek(): ParameterBag
+    {
+        return $this->naglowek;
+    }
+
+    public function setNaglowek(ParameterBag $naglowek): static
+    {
+        $this->naglowek = $naglowek;
+        return $this;
+    }
+
+    public function getPozycjeSzczegolowe(): ParameterBag
+    {
+        return $this->pozycjeSzczegolowe;
+    }
+
+    public function setPozycjeSzczegolowe(ParameterBag $pozycjeSzczegolowe): static
+    {
+        $this->pozycjeSzczegolowe = $pozycjeSzczegolowe;
+        return $this;
+    }
+
+    public function getPouczenia(): ParameterBag
+    {
+        return $this->pouczenia;
+    }
+
+    public function setPouczenia(ParameterBag $pouczenia): static
+    {
+        $this->pouczenia = $pouczenia;
+        return $this;
+    }
+}
---
+++ src/Model/NaglowekDeklaracja.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+/**
+ * @author Jakub Gniecki <jgniecki.contact@gmail.com>
+ * @copyright
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DevLancer\JPKGenerator\Model;
+
+use DevLancer\JPKGenerator\ParameterBag;
+
+final class NaglowekDeklaracja
+{
+    private ParameterBag $kodFormularzaDekl;
+    private ParameterBag $wariantFormularzaDekl;
+    public function __construct()
+    {
+        $this->kodFormularzaDekl = new ParameterBag(
+            "KodFormularzaDekl",
+            "string",
+            false,
+            [
+                "kodPodatku",
+                "kodSystemowy",
+                "rodzajZobowiazania",
+                "wersjaSchemy"
+            ]
+        );
+
+        $this->wariantFormularzaDekl = new ParameterBag(
+            "WariantFormularzaDekl",
+            "int",
+            false,
+        );
+    }
+
+    public function getKodFormularzaDekl(): ParameterBag
+    {
+        return $this->kodFormularzaDekl;
+    }
+
+    public function setKodFormularzaDekl(ParameterBag $kodFormularzaDekl): static
+    {
+        $this->kodFormularzaDekl = $kodFormularzaDekl;
+        return $this;
+    }
+
+    public function getWariantFormularzaDekl(): ParameterBag
+    {
+        return $this->wariantFormularzaDekl;
+    }
+
+    public function setWariantFormularzaDekl(ParameterBag $wariantFormularzaDekl): static
+    {
+        $this->wariantFormularzaDekl = $wariantFormularzaDekl;
+        return $this;
+    }
+}
