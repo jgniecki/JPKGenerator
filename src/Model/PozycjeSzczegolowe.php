85bfa3a6e7bbdbe249fef6f33961d8a801cad56f
Jakub Gniecki <jgniecki.contact@gmail.com>
2025-09-03 08:36:16 +0200

    Dodano nowe P

--- src/Model/PozycjeSzczegolowe.php
+++ src/Model/PozycjeSzczegolowe.php
@@ -12,8 +12,15 @@ use DevLancer\JPKGenerator\ParameterBag;
 
 final  class PozycjeSzczegolowe
 {
+    private ParameterBag $p_10;
     private ParameterBag $p_11;
     private ParameterBag $p_12;
+    private ParameterBag $p_13;
+    private ParameterBag $p_14;
+    private ParameterBag $p_15;
+    private ParameterBag $p_16;
+    private ParameterBag $p_17;
+    private ParameterBag $p_18;
     private ParameterBag $p_19;
     private ParameterBag $p_20;
     private ParameterBag $p_21;
@@ -21,23 +28,73 @@ final  class PozycjeSzczegolowe
     private ParameterBag $p_23;
     private ParameterBag $p_24;
     private ParameterBag $p_25;
+    private ParameterBag $p_26;
     private ParameterBag $p_27;
+    private ParameterBag $p_28;
     private ParameterBag $p_29;
+    private ParameterBag $p_30;
     private ParameterBag $p_31;
+    private ParameterBag $p_32;
     private ParameterBag $p_33;
     private ParameterBag $p_34;
     private ParameterBag $p_35;
     private ParameterBag $p_36;
     private ParameterBag $p_37;
     private ParameterBag $p_38;
+    private ParameterBag $p_39;
+    private ParameterBag $p_40;
+    private ParameterBag $p_41;
+    private ParameterBag $p_42;
+    private ParameterBag $p_43;
+    private ParameterBag $p_44;
+    private ParameterBag $p_45;
+    private ParameterBag $p_46;
+    private ParameterBag $p_47;
+    private ParameterBag $p_48;
+    private ParameterBag $p_49;
+    private ParameterBag $p_50;
+    private ParameterBag $p_51;
+    private ParameterBag $p_52;
+    private ParameterBag $p_53;
+    private ParameterBag $p_62;
+    private ParameterBag $p_68;
+    private ParameterBag $p_69;
     public function __construct()
     {
+        $this->p_10 = new ParameterBag(
+            "P_10",
+            "int",
+        );
         $this->p_11 = new ParameterBag(
             "P_11",
             "int",
         );
         $this->p_12 = new ParameterBag(
             "P_12",
+            "int",
+        );
+        $this->p_13 = new ParameterBag(
+            "P_13",
+            "int",
+        );
+        $this->p_14 = new ParameterBag(
+            "P_14",
+            "int",
+        );
+        $this->p_15 = new ParameterBag(
+            "P_15",
+            "int",
+        );
+        $this->p_16 = new ParameterBag(
+            "P_16",
+            "int",
+        );
+        $this->p_17 = new ParameterBag(
+            "P_17",
+            "int",
+        );
+        $this->p_18 = new ParameterBag(
+            "P_18",
             "int",
         );
         $this->p_19 = new ParameterBag(
@@ -68,16 +125,32 @@ final  class PozycjeSzczegolowe
             "P_25",
             "int",
         );
+        $this->p_26 = new ParameterBag(
+            "P_26",
+            "int",
+        );
         $this->p_27 = new ParameterBag(
             "P_27",
+            "int",
+        );
+        $this->p_28 = new ParameterBag(
+            "P_28",
             "int",
         );
         $this->p_29 = new ParameterBag(
             "P_29",
             "int",
         );
+        $this->p_30 = new ParameterBag(
+            "P_30",
+            "int",
+        );
         $this->p_31 = new ParameterBag(
             "P_31",
+            "int",
+        );
+        $this->p_32 = new ParameterBag(
+            "P_32",
             "int",
         );
         $this->p_33 = new ParameterBag(
@@ -104,6 +177,89 @@ final  class PozycjeSzczegolowe
             "P_38",
             "int",
         );
+        $this->p_39 = new ParameterBag(
+            "P_39",
+            "int",
+        );
+        $this->p_40 = new ParameterBag(
+            "P_40",
+            "int",
+        );
+        $this->p_41 = new ParameterBag(
+            "P_41",
+            "int",
+        );
+        $this->p_42 = new ParameterBag(
+            "P_42",
+            "int",
+        );
+        $this->p_43 = new ParameterBag(
+            "P_43",
+            "int",
+        );
+        $this->p_44 = new ParameterBag(
+            "P_44",
+            "int",
+        );
+        $this->p_45 = new ParameterBag(
+            "P_45",
+            "int",
+        );
+        $this->p_46 = new ParameterBag(
+            "P_46",
+            "int",
+        );
+        $this->p_47 = new ParameterBag(
+            "P_47",
+            "int",
+        );
+        $this->p_48 = new ParameterBag(
+            "P_48",
+            "int",
+        );
+        $this->p_49 = new ParameterBag(
+            "P_49",
+            "int",
+        );
+        $this->p_50 = new ParameterBag(
+            "P_50",
+            "int",
+        );
+        $this->p_51 = new ParameterBag(
+            "P_51",
+            "int",
+        );
+        $this->p_52 = new ParameterBag(
+            "P_52",
+            "int",
+        );
+        $this->p_53 = new ParameterBag(
+            "P_53",
+            "int",
+        );
+        $this->p_62 = new ParameterBag(
+            "P_62",
+            "int",
+        );
+        $this->p_68 = new ParameterBag(
+            "P_68",
+            "int",
+        );
+        $this->p_69 = new ParameterBag(
+            "P_69",
+            "int",
+        );
+    }
+
+    public function getP10(): ParameterBag
+    {
+        return $this->p_10;
+    }
+
+    public function setP10(ParameterBag $p_10): static
+    {
+        $this->p_10 = $p_10;
+        return $this;
     }
 
     public function getP11(): ParameterBag
@@ -125,6 +281,72 @@ final  class PozycjeSzczegolowe
     public function setP12(ParameterBag $p_12): static
     {
         $this->p_12 = $p_12;
+        return $this;
+    }
+
+    public function getP13(): ParameterBag
+    {
+        return $this->p_13;
+    }
+
+    public function setP13(ParameterBag $p_13): static
+    {
+        $this->p_13 = $p_13;
+        return $this;
+    }
+
+    public function getP14(): ParameterBag
+    {
+        return $this->p_14;
+    }
+
+    public function setP14(ParameterBag $p_14): static
+    {
+        $this->p_14 = $p_14;
+        return $this;
+    }
+
+    public function getP15(): ParameterBag
+    {
+        return $this->p_15;
+    }
+
+    public function setP15(ParameterBag $p_15): static
+    {
+        $this->p_15 = $p_15;
+        return $this;
+    }
+
+    public function getP16(): ParameterBag
+    {
+        return $this->p_16;
+    }
+
+    public function setP16(ParameterBag $p_16): static
+    {
+        $this->p_16 = $p_16;
+        return $this;
+    }
+
+    public function getP17(): ParameterBag
+    {
+        return $this->p_17;
+    }
+
+    public function setP17(ParameterBag $p_17): static
+    {
+        $this->p_17 = $p_17;
+        return $this;
+    }
+
+    public function getP18(): ParameterBag
+    {
+        return $this->p_18;
+    }
+
+    public function setP18(ParameterBag $p_18): static
+    {
+        $this->p_18 = $p_18;
         return $this;
     }
 
@@ -205,6 +427,17 @@ final  class PozycjeSzczegolowe
         return $this;
     }
 
+    public function getP26(): ParameterBag
+    {
+        return $this->p_26;
+    }
+
+    public function setP26(ParameterBag $p_26): static
+    {
+        $this->p_26 = $p_26;
+        return $this;
+    }
+
     public function getP27(): ParameterBag
     {
         return $this->p_27;
@@ -213,6 +446,17 @@ final  class PozycjeSzczegolowe
     public function setP27(ParameterBag $p_27): static
     {
         $this->p_27 = $p_27;
+        return $this;
+    }
+
+    public function getP28(): ParameterBag
+    {
+        return $this->p_28;
+    }
+
+    public function setP28(ParameterBag $p_28): static
+    {
+        $this->p_28 = $p_28;
         return $this;
     }
 
@@ -227,6 +471,17 @@ final  class PozycjeSzczegolowe
         return $this;
     }
 
+    public function getP30(): ParameterBag
+    {
+        return $this->p_30;
+    }
+
+    public function setP30(ParameterBag $p_30): static
+    {
+        $this->p_30 = $p_30;
+        return $this;
+    }
+
     public function getP31(): ParameterBag
     {
         return $this->p_31;
@@ -235,6 +490,17 @@ final  class PozycjeSzczegolowe
     public function setP31(ParameterBag $p_31): static
     {
         $this->p_31 = $p_31;
+        return $this;
+    }
+
+    public function getP32(): ParameterBag
+    {
+        return $this->p_32;
+    }
+
+    public function setP32(ParameterBag $p_32): static
+    {
+        $this->p_32 = $p_32;
         return $this;
     }
 
@@ -301,6 +567,204 @@ final  class PozycjeSzczegolowe
     public function setP38(ParameterBag $p_38): static
     {
         $this->p_38 = $p_38;
+        return $this;
+    }
+
+    public function getP39(): ParameterBag
+    {
+        return $this->p_39;
+    }
+
+    public function setP39(ParameterBag $p_39): static
+    {
+        $this->p_39 = $p_39;
+        return $this;
+    }
+
+    public function getP40(): ParameterBag
+    {
+        return $this->p_40;
+    }
+
+    public function setP40(ParameterBag $p_40): static
+    {
+        $this->p_40 = $p_40;
+        return $this;
+    }
+
+    public function getP41(): ParameterBag
+    {
+        return $this->p_41;
+    }
+
+    public function setP41(ParameterBag $p_41): static
+    {
+        $this->p_41 = $p_41;
+        return $this;
+    }
+
+    public function getP42(): ParameterBag
+    {
+        return $this->p_42;
+    }
+
+    public function setP42(ParameterBag $p_42): static
+    {
+        $this->p_42 = $p_42;
+        return $this;
+    }
+
+    public function getP43(): ParameterBag
+    {
+        return $this->p_43;
+    }
+
+    public function setP43(ParameterBag $p_43): static
+    {
+        $this->p_43 = $p_43;
+        return $this;
+    }
+
+    public function getP44(): ParameterBag
+    {
+        return $this->p_44;
+    }
+
+    public function setP44(ParameterBag $p_44): static
+    {
+        $this->p_44 = $p_44;
+        return $this;
+    }
+
+    public function getP45(): ParameterBag
+    {
+        return $this->p_45;
+    }
+
+    public function setP45(ParameterBag $p_45): static
+    {
+        $this->p_45 = $p_45;
+        return $this;
+    }
+
+    public function getP46(): ParameterBag
+    {
+        return $this->p_46;
+    }
+
+    public function setP46(ParameterBag $p_46): static
+    {
+        $this->p_46 = $p_46;
+        return $this;
+    }
+
+    public function getP47(): ParameterBag
+    {
+        return $this->p_47;
+    }
+
+    public function setP47(ParameterBag $p_47): static
+    {
+        $this->p_47 = $p_47;
+        return $this;
+    }
+
+    public function getP48(): ParameterBag
+    {
+        return $this->p_48;
+    }
+
+    public function setP48(ParameterBag $p_48): static
+    {
+        $this->p_48 = $p_48;
+        return $this;
+    }
+
+    public function getP49(): ParameterBag
+    {
+        return $this->p_49;
+    }
+
+    public function setP49(ParameterBag $p_49): static
+    {
+        $this->p_49 = $p_49;
+        return $this;
+    }
+
+    public function getP50(): ParameterBag
+    {
+        return $this->p_50;
+    }
+
+    public function setP50(ParameterBag $p_50): static
+    {
+        $this->p_50 = $p_50;
+        return $this;
+    }
+
+    public function getP51(): ParameterBag
+    {
+        return $this->p_51;
+    }
+
+    public function setP51(ParameterBag $p_51): static
+    {
+        $this->p_51 = $p_51;
+        return $this;
+    }
+
+    public function getP52(): ParameterBag
+    {
+        return $this->p_52;
+    }
+
+    public function setP52(ParameterBag $p_52): static
+    {
+        $this->p_52 = $p_52;
+        return $this;
+    }
+
+    public function getP53(): ParameterBag
+    {
+        return $this->p_53;
+    }
+
+    public function setP53(ParameterBag $p_53): static
+    {
+        $this->p_53 = $p_53;
+        return $this;
+    }
+
+    public function getP62(): ParameterBag
+    {
+        return $this->p_62;
+    }
+
+    public function setP62(ParameterBag $p_62): static
+    {
+        $this->p_62 = $p_62;
+        return $this;
+    }
+
+    public function getP68(): ParameterBag
+    {
+        return $this->p_68;
+    }
+
+    public function setP68(ParameterBag $p_68): static
+    {
+        $this->p_68 = $p_68;
+        return $this;
+    }
+
+    public function getP69(): ParameterBag
+    {
+        return $this->p_69;
+    }
+
+    public function setP69(ParameterBag $p_69): static
+    {
+        $this->p_69 = $p_69;
         return $this;
     }
 }
